a358e29346aa8783a13020e21b0beb2f1d95c194
Collin Winter <collinw@gmail.com>
2018-12-28 09:30:13 -0800

    Use rollup's TS plugin.

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript';
 
 export default {
-	input: 'build/src/index.js',
-	output: {
+  input: 'src/index.ts',
+  output: {
     name: 'main',
-		file: 'build/bundle.js',
-		format: 'iife',
-		sourcemap: true
-	},
-	plugins: [
-		resolve(),  // tells Rollup how to find date-fns in node_modules
-		commonjs()  // converts date-fns to ES modules
-	]
+    file: 'build/bundle.js',
+    format: 'iife',
+    sourcemap: true
+  },
+  plugins: [
+    typescript(),
+    resolve(),
+    commonjs()
+  ]
 };
