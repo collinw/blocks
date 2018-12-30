01df77bfe7e976f5a3b702d489379696c49383e0
Collin Winter <collinw@gmail.com>
2018-12-29 16:24:54 -0800

    Add tests for GetPlayerInputs

--- src/blocks.spec.ts
+++ src/blocks.spec.ts
@@ -68,4 +68,29 @@ describe('GameState', () => {
     ]);
     expect(result).to.deep.equal(new blocks.CoordSet([0, 0], [5, 8]));
   });
+
+  describe('GetPlayerInputs', () => {
+
+    it('should work', () => {
+      // Deliberately position both players on the edge of the board so that we verify
+      // no off-the-board positions are generated.
+      const state = new blocks.GameState();
+      state.board.Set(0, 1, 2);
+      state.board.Set(0, 0, 3);
+
+      // A player with no pieces on the board. In the real game, we
+      // simply hardcode the initial set of inputs for each player.
+      let inputs = blocks.GetPlayerInputs(state, 1);
+      expect(inputs.startPoints.Size()).to.equal(0);
+      expect(inputs.exclude).to.deep.equal(new blocks.CoordSet([0, 0], [0, 1]));
+
+      inputs = blocks.GetPlayerInputs(state, 2);
+      expect(inputs.startPoints).to.deep.equal(new blocks.CoordSet([1, 0], [1, 2]));
+      expect(inputs.exclude).to.deep.equal(new blocks.CoordSet([0, 0], [0, 1], [0, 2], [1, 1]));
+
+      inputs = blocks.GetPlayerInputs(state, 3);
+      expect(inputs.startPoints).to.deep.equal(new blocks.CoordSet([1, 1]));
+      expect(inputs.exclude).to.deep.equal(new blocks.CoordSet([0, 0], [0, 1], [1, 0]));
+    });
+  });
 });
