3c7d8d500bddda8918b4914b1c9297359ad49d61
Collin Winter <collinw@gmail.com>
2018-12-30 09:08:12 -0800

    Restructure UI code to make profiling easier

--- src/ui.ts
+++ src/ui.ts
@@ -6,7 +6,7 @@ function CellId(m: number, n: number): string {
   return 'cell' + m + 'x' + n;
 }
 
-export function Draw(state: blocks.GameState) {
+function DrawBoard(state: blocks.GameState) {
   const board = $('#board');
   board.empty();
 
@@ -18,15 +18,37 @@ export function Draw(state: blocks.GameState) {
     }
     board.append(row);
   }
+}
 
-  const scores = blocks.GetScores(state);
-  for (let i = 1; i <= 4; i++) {
-    const elem = $('#player-score-' + i);
-    elem.text(scores[i]);
+function DrawRemainingPieces(player: blocks.Player) {
+  const playerUI = $('#pieces-player' + player.id);
+  playerUI.empty();
+
+  for (const piece of player.pieces) {
+    const pieceUI = $('<table class=\'piece\'></table>');
+    for (let m = 0; m < piece.canonical.M; m++) {
+      const row = $('<tr></tr>');
+      for (let n = 0; n < piece.canonical.N; n++) {
+        const cell = $('<td></td>');
+        if (piece.canonical.Get(m, n) > 0) {
+          cell.addClass('piece-unit');
+        }
+        row.append(cell);
+      }
+      pieceUI.append(row);
+    }
+    playerUI.append(pieceUI);
   }
+}
+
+function DrawPlayerTable(state: blocks.GameState) {
+  const scores = blocks.GetScores(state);
 
   for (let i = 0; i < 4; i++) {
     const player = state.players[i];
+
+    const elem = $('#player-score-' + player.id);
+    elem.text(scores[player.id]);
 
     const desc = player.agent.Description();
     $(".player" + player.id + " > .player-desc").text(desc);
@@ -39,23 +61,12 @@ export function Draw(state: blocks.GameState) {
       cell.text('x');
     }
 
-    const playerUI = $('#pieces-player' + player.id);
-    playerUI.empty();
-
-    for (const piece of player.pieces) {
-      const pieceUI = $('<table class=\'piece\'></table>');
-      for (let m = 0; m < piece.canonical.M; m++) {
-        const row = $('<tr></tr>');
-        for (let n = 0; n < piece.canonical.N; n++) {
-          const cell = $('<td></td>');
-          if (piece.canonical.Get(m, n) > 0) {
-            cell.addClass('piece-unit');
-          }
-          row.append(cell);
-        }
-        pieceUI.append(row);
-      }
-      playerUI.append(pieceUI);
-    }
+    DrawRemainingPieces(player);
   }
+}
+
+
+export function Draw(state: blocks.GameState) {
+  DrawBoard(state);
+  DrawPlayerTable(state);
 }
